5a6caa74e256366f1f16f8b5fe464dcc4231b512
Levi <levifernandes@usp.br>
2020-02-11 21:34:25 -0300

    something

---
+++ src/config/database.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+module.exports = {
+    development: {
+        use_env_variable: 'postgres://lzgnwllg:xBz2Lb4Qdm7gt0Kyw574vwvFkySig7to@rajje.db.elephantsql.com:5432/lzgnwllg'
+    },
+}
